c12a5bcc241730e97a029b458fca92e21c939f29
Anna Reser <anna-reser@uiowa.edu>
2023-03-29 18:49:22 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+all_books_dict = [
+    {"Title": "The Hobbit", "Author": "J.R.R. Tolkien", "Pages": 295, 
+     "Classification": "fiction", "Details": "read, recommend", "Acquisition": "library",},
+]
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template(
+        "index.html", pageTitle="Add a book to my library", books=all_books_dict)
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    print("inside add function")
+    if request.method == "POST":
+
+        form = request.form
+
+        Title = form["Title"]
+        Author = form["Author"]
+        Pages = form["Pages"]
+        Classification = form["Classification"]
+        Details = form.getlist("Details")
+        Acquisition = form["Acquisition"] #this is a Python list 
+
+        print(Title)
+        print(Author)
+        print(Pages)
+        print(Classification)
+        print (Details)
+        print(Acquisition)
+
+
+        details_string = ", ".join(Details)  # make the Python list into a string
+
+        book_dict = {
+            "Title": Title,
+            "Author": Author,
+            "Pages": Pages,
+            "Classification": Classification,
+            "Details": details_string,
+            "Acquisition": Acquisition,
+        }
+
+        print(book_dict)
+        all_books_dict.append(
+            book_dict
+        )  # append this dictionary entry to the larger friends dictionary
+        print(all_books_dict)
+        return redirect(url_for("index"))
+    else:
+        return redirect(url_for("index"))
+    
+@app.route("/about", methods=["GET", "POST"])
+def about():
+    return render_template(
+        "about.html", pageTitle="About", books=all_books_dict)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
